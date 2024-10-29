a05baf4e9a370c31f068229c73bc5710b835fce1
ArdaHalim2 <arda.halim@iths.se>
2024-10-29 12:44:14 +0100

    initial commit

--- src/main/java/com/example/pumpkin/controller/HelloController.java
+++ src/main/java/com/example/pumpkin/controller/HelloController.java
@@ -1,36 +1,85 @@
 package com.example.pumpkin.controller;
 
+import com.example.pumpkin.model.GameState;
 import com.example.pumpkin.model.Model;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.util.Duration;
 
 public class HelloController {
 
+    public Canvas canvas;
+
     //This method will run after the controller object is created
     //and the fields are initialized.
     public void initialize() {
-        textFieldTarget.textProperty().bind(model.userTextProperty());
-        //transferButton.disableProperty().bind(textField.textProperty().isEmpty());
+        startRandomEvent();
     }
 
-    private Model model = new Model();
+    private void startRandomEvent() {
+        Timeline timeline = new Timeline(
+                new KeyFrame(
+                        Duration.millis(300),
+                        (ActionEvent event) -> {
+                            gameUpdate();
+                            startRandomEvent();
+                        }
+                )
+        );
+        timeline.play();
+    }
 
-    public Button transferButton;
+    public void keyPressed(KeyEvent keyEvent) {
+        //Call methods on model to update snake direction
+        switch (keyEvent.getCode()) {
+            case UP, W -> model.setUp();
+            case DOWN, S -> model.setDown();
+            case LEFT, A -> model.setLeft();
+            case RIGHT, D -> model.setRight();
+            case SPACE -> model.pauseUnpause();
+        }
+    }
 
-    public TextField textField;
-
-    public TextField textFieldTarget;
+    private final Model model = new Model();
 
     public Model getModel() {
         return model;
     }
 
-    @FXML
-    protected void transferButtonAction() {
-        model.setUserText(textField.getText());
+    public void gameUpdate() {
+        model.update();
+
+        var gc = canvas.getGraphicsContext2D();
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        renderSnake(gc);
+        renderApple(gc);
+        renderGameState(gc);
+        model.setRenderedSinceLastDirectionChange();
+    }
+
+    private void renderGameState(GraphicsContext gc) {
+        gc.setFill(Color.BLACK);
+        gc.setFont(new Font(20));
+
+        if (model.getGameState() == GameState.PAUSED)
+            gc.fillText("Press Space to start playing", 200, 300);
+    }
+
+    private void renderSnake(GraphicsContext gc) {
+        gc.setFill(Color.GREEN);
+        model.getSnake().forEach(point -> {
+            gc.fillRect(point.x() - 10, point.y() - 10, 20, 20);
+        });
+    }
+
+    private void renderApple(GraphicsContext gc) {
+        gc.setFill(Color.RED);
+        gc.fillOval(model.getApple().x() - 10, model.getApple().y() - 10, 20, 20);
     }
 }
--- src/main/java/com/example/pumpkin/model/Model.java
+++ src/main/java/com/example/pumpkin/model/Model.java
@@ -1,21 +1,131 @@
 package com.example.pumpkin.model;
 
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static com.example.pumpkin.model.Direction.*;
+import static com.example.pumpkin.model.GameState.*;
 
 public class Model {
-    private StringProperty userText = new SimpleStringProperty();
+    public static final int BOARD_SIZE = 600;
+    Point apple;
+    //Point head; snake.getFirst()
+    List<Point> snake = new ArrayList<>();
+    Direction currentDirection = UP;
+    GameState gameState = PAUSED;
+    Random random = new Random();
+    boolean renderedSinceLastDirectionChange = true;
 
-    public String getUserText() {
-        return userText.get();
+    public Model() {
+        snake.add(new Point(310, 310));
+        snake.add(new Point(310, 330));
+        snake.add(new Point(310, 350));
+
+        apple = randomApple(snake.getFirst());
     }
 
-    public StringProperty userTextProperty() {
-        return userText;
+    public Direction getCurrentDirection() {
+        return currentDirection;
     }
 
-    public void setUserText(String userText) {
-        if (!userText.isEmpty())
-            this.userText.set(userText);
+    public void setRenderedSinceLastDirectionChange() {
+        this.renderedSinceLastDirectionChange = true;
+    }
+
+    public List<Point> getSnake() {
+        return snake;
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+    public void pauseUnpause() {
+        if( gameState == RUNNING)
+            gameState = PAUSED;
+        else if (gameState == PAUSED)
+            gameState = RUNNING;
+    }
+
+    public void update() {
+        if (gameState != RUNNING) {
+            return;
+        }
+
+        Point next = calculateNextHeadPos();
+        checkForCollisionWithWalls(next);
+        checkForCollisionWithApple(next);
+        checkForCollisionWithSelf(next);
+        //Spara ny position
+        snake.addFirst(next);
+    }
+
+    private Point calculateNextHeadPos() {
+        var currentHead = snake.getFirst();
+        return switch (currentDirection) {
+            case LEFT -> new Point(currentHead.x() - 20, currentHead.y());
+            case RIGHT -> new Point(currentHead.x() + 20, currentHead.y());
+            case UP -> new Point(currentHead.x(), currentHead.y() - 20);
+            case DOWN -> new Point(currentHead.x(), currentHead.y() + 20);
+        };
+    }
+
+    private void checkForCollisionWithWalls(Point next) {
+        if (next.x() < 10 || next.x() > BOARD_SIZE - 10 ||
+                next.y() < 10 || next.y() > BOARD_SIZE - 10) {
+            gameState = FINISHED;
+        }
+    }
+
+    private void checkForCollisionWithSelf(Point next) {
+        if (snake.contains(next))  //Compare objects using equals... what happens with double values?????
+            gameState = FINISHED;
+    }
+
+    private void checkForCollisionWithApple(Point next) {
+        if (next.equals(apple))
+            apple = randomApple(next);
+        else
+            snake.removeLast();
+    }
+
+    private Point randomApple(Point next) {
+        Point newApple;
+        do {
+            int xPos = random.nextInt(BOARD_SIZE / 20) * 20 + 10;
+            int yPos = random.nextInt(BOARD_SIZE / 20) * 20 + 10;
+            //10 30 50 70 90 ... 590
+            newApple = new Point(xPos, yPos);
+        } while (newApple.equals(next) || snake.contains(newApple));
+        return newApple;
+    }
+
+    public void setUp() {
+        if (currentDirection != DOWN && renderedSinceLastDirectionChange)
+            currentDirection = UP;
+        renderedSinceLastDirectionChange = false;
+    }
+
+    public void setDown() {
+        if (currentDirection != UP && renderedSinceLastDirectionChange)
+            currentDirection = DOWN;
+        renderedSinceLastDirectionChange = false;
+    }
+
+    public void setLeft() {
+        if (currentDirection != RIGHT && renderedSinceLastDirectionChange)
+            currentDirection = LEFT;
+        renderedSinceLastDirectionChange = false;
+    }
+
+    public void setRight() {
+        if (currentDirection != LEFT && renderedSinceLastDirectionChange)
+            currentDirection = RIGHT;
+        renderedSinceLastDirectionChange = false;
+    }
+
+    public Point getApple() {
+        return apple;
     }
 }
